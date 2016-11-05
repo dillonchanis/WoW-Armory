f2dc3d26c127bf1b90b0b2a775aeef82591193d9
Dillon Chanis <dillonchanis92@gmail.com>
2016-11-05 13:31:51 -0400

    Add API Endpoint for Spell lookup

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const axios = require('axios');
+
+const bodyParser = require('body-parser');
+
+const api = require('./api.config.js')();
+
+app.use(express.static(path.resolve('wow-armory', '..', 'build')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.get('*', function(req, res) {
+	res.sendFile(path.resolve('wow-armory', '..', 'build', 'index.html'));
+});
+
+app.post('/', function(req, res) {
+
+		var url = `${api.baseURL}${req.body.realm}/${req.body.name}?fields=items,stats&locale=en_US&apikey=${api.key}`;
+
+		axios.get(url)
+				 .then(function(response) {
+				 		res.json(response.data);
+				 })
+				 .catch(function(error) {
+				 		return error;
+				 });
+
+});
+
+app.post('/api/spell', function(req, res) {
+
+		var url = `https://us.api.battle.net/wow/spell/${req.body.spellId}?locale=en_US&apikey=${api.key}`;
+
+		axios.get(url)
+				 .then(function(response) {
+				 		res.json(response.data);
+				 })
+				 .catch(function(error) {
+				 		return error;
+				 });
+				 
+});
+
+app.listen(process.env.PORT || 9000, function() {
+	console.log('Running...');
+});
